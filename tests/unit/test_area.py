47444f993045e4e4a0d2b1c45969a11d87b9914f
rithikasriram03-cpu <rithika.sriram03@gmail.com>
2026-06-22 14:16:02 +0530

    Fix syntax error in test function definition

--- tests/unit/test_area.py
+++ tests/unit/test_area.py
@@ -10,7 +10,7 @@ def test_calculate_area_square_negative():
     with pytest.raises(TypeError):
         calculate_area_square(-2)
 
-def test_calculate_area_square_string():
+def test_calculate_area_square_string(:
     with pytest.raises(TypeError):
         calculate_area_square("2")
 
