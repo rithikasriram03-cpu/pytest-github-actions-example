b65e0fcb39e90374ba354c1727e154bf9902960a
rithikasriram03-cpu <rithika.sriram03@gmail.com>
2026-07-06 17:01:48 +0530

    Fix test to check string input for negative area

--- tests/unit/test_area.py
+++ tests/unit/test_area.py
@@ -9,7 +9,7 @@ def test_calculate_area_square():
 
 def test_calculate_area_square_negative():
     with pytest.raises(TypeError):
-        calculate_area_square(-2)
+        calculate_area_square("-2");
 
 
 def test_calculate_area_square_string():
